048a1b368d2c88a176e62c0055022ed47bed8ef1
mkdk <eugen_k69@yahoo.com>
2018-10-19 11:17:16 +0300

    Initial Commit

---
+++ demoissumacaw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demoissumacaw
+//
+//  Created by Eugen K on 19/10/2018.
+//  Copyright © 2018 Eugen K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
